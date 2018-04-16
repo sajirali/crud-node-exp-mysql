fc75ef0fe83e2096d4f6d6ff6789f8d5c85a6c4e
Sajir Ali <sajirali@gmail.com>
2018-04-15 22:15:03 -0400

    simple crud node express mysql app ready out of the box

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+    database: {
+        host: 'localhost',      // database host
+        user: 'root',           // your database username
+        password: 'trinimbus',  // your database password
+        port: 3306,             // default MySQL port
+        db: 'mysqlnodeexp'      // your database name
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '80'
+    }
+}
+
+module.exports = config
